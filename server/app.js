2a88a0ad46134b716789d5983ed799bc85e5a5b3
Ravan <iamdinarbey@gmail.com>
2023-12-03 00:22:04 +0300

    added register and login

--- server/app.js
+++ server/app.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ValidationError } = require('express-validation');
 
 const app = express();
 
 app.use(bodyParser.json()) // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false })) // for parsing application/x-www-form-urlencoded
 
 app.use('/auth', require('./controllers/auth'));
+
+app.use(function (err, req, res, next) {
+    if (err instanceof ValidationError) {
+        return res.status(err.statusCode).json(err)
+    }
+    return res.status(400).json(err)
+})
+
 
 const port = 3005;
 
