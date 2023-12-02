c7bf3da926a215ff2e5734540a8368fa7b874ecb
Ravan <iamdinarbey@gmail.com>
2023-12-03 00:22:51 +0300

    added register and login with middleware

--- client/src/app/login/page.js
+++ client/src/app/login/page.js
@@ -26,7 +26,7 @@ export default function Login()
                                                className="block text-sm font-medium leading-6 text-gray-900">E-posta
                                             adresi yada Kullanıcı adı</label>
                                         <div className="mt-2">
-                                            <input id="email" name="email" type="email" autoComplete="email" required=""
+                                            <input id="email" name="email" type="text" autoComplete="email" required=""
                                                    className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"/>
                                         </div>
                                     </div>
---
+++ client/src/middleware/isAuth.js
@@ -0,0 +1,9 @@
+export const isAuth = (cookies) => {
+
+    if (cookies.has('accessToken')) {
+        return true;
+    }
+
+    return false;
+
+}
