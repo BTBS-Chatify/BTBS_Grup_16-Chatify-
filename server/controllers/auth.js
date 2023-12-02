5d2057682b972514b989a6f7a111b54dc64c65f8
Ravan <iamdinarbey@gmail.com>
2023-12-03 00:21:53 +0300

    added register and login

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -3,16 +3,111 @@
 const express = require('express');
 var route = express.Router();
 
+const { validate } = require("express-validation");
+
+const authValidation = require('../validations/authValidations');
+
 const { PrismaClient } = require('@prisma/client');
+const {isEmail} = require("validator");
 
 const prisma = new PrismaClient();
 
-route.post('/register',  async (req, res, next) => {
+route.post('/register', validate(authValidation.create, {}, {}), async (request, response, next) => {
 
-    const users = await prisma.user.findMany()
+    try {
 
-    console.log(users)
+        const emailExists = await prisma.user.findFirst({
+            where: {
+                OR: [
+                    { email: request.body.email },
+                    { username: request.body.username }
+                ]
+            },
+        })
+
+        if (emailExists) {
+            return response.status(400).json({
+                "error":"User already registered"
+            })
+        }
+
+        const user = await prisma.user.create({
+            data: {
+                email: request.body.email,
+                username: request.body.username,
+                password: request.body.password,
+                name: "Hosgeldin Muzaffer"
+            }
+        })
+
+        return response.status(200).json({
+            "status": "success",
+            "message":"Kayit basarili qral",
+            "data": {
+                user
+            }
+        })
+
+    } catch (err) {
+        return response.status(400).json({
+            status: 'error',
+            message: err,
+        })
+    }
 
 });
+
+route.post('/login', validate(authValidation.login, {}, {}), async (request, response, next) => {
+
+    var emailOrUsername = true;
+    var userExists = null;
+
+    if (!isEmail(request.body.email)) {
+        emailOrUsername = false;
+    }
+
+    try {
+
+        if (emailOrUsername == true) {
+            var userExists = await prisma.user.findFirst({
+                where: {
+                    email: request.body.email,
+                    password: request.body.password
+                }
+            })
+        } else {
+            var userExists = await prisma.user.findFirst({
+                where: {
+                    username: request.body.email,
+                    password: request.body.password
+                }
+            })
+        }
+
+        if (!userExists)
+        {
+            return response.status(400).json({
+                "error":"User does not exist"
+            })
+        }
+
+        return response.status(200).json({
+            "status": "success",
+            "message":"giris basarili",
+            "data": {
+                userExists
+            }
+        })
+
+    } catch (err) {
+        return response.status(400).json({
+            status: 'error',
+            message: err,
+        })
+    }
+
+});
+
+
 
 module.exports = route;
---
+++ server/validations/authValidations.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const { Joi } = require('express-validation')
+
+const { PrismaClient } = require('@prisma/client');
+
+const prismaClient = new PrismaClient();
+
+module.exports = {
+    create: {
+        body: Joi.object({
+            email: Joi.string()
+                .email({ minDomainSegments: 2 })
+                // .custom(async value => {
+                //     const exists = await prismaClient.user.findUnique({
+                //         where: {
+                //             email: value
+                //         }
+                //     });
+                //
+                //     if (exists) {
+                //         throw new Error('E-mail already in use');
+                //     }
+                //
+                // })
+                .required(),
+            username: Joi.string()
+                .alphanum()
+                .min(3)
+                .max(30)
+                .required(),
+            password: Joi.string()
+                .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
+                .required(),
+            // passwordConfirmation: Joi.string()
+            //     .custom((value, { req }) => {
+            //         return value === req.body.password
+            //     })
+            // .required()
+        })
+    },
+    login: {
+        body: Joi.object({
+            email: Joi.string()
+                .required(),
+            password: Joi.string()
+                .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+        })
+    }
+}
