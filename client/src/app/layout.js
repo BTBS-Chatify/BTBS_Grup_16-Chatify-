866c063e35fcff713e44714a2c94c45be14e7329
Ravan <iamdinarbey@gmail.com>
2023-12-04 01:55:59 +0300

    added toast container

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from 'next/font/google'
+import { ToastContainer } from 'react-toastify';
 import './globals.css'
+import 'react-toastify/dist/ReactToastify.css';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +13,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="h-full">
-      <body className="h-full">{children}</body>
+      <body className="h-full">
+      {children}
+      <ToastContainer />
+      </body>
     </html>
   )
 }
