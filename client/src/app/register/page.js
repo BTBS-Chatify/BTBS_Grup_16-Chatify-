568cb36737bd87c9eee0751afe18f8dd4bf15cff
Ravan <iamdinarbey@gmail.com>
2023-12-03 14:37:28 +0300

    added cors and form submit

--- client/src/app/register/page.js
+++ client/src/app/register/page.js
@@ -1,6 +1,53 @@
+"use client";
+
 import Link from "next/link";
-export default function Login()
+import { useState } from "react";
+import axios from "axios";
+
+export default function Register()
 {
+
+    // const [candidat, setCandidat] = useState({
+    //
+    // })
+
+    // const onSubmit = async (e) => {
+    //     e.preventDefault();
+    //
+    //     console.log("Register")
+    //
+    //     // await fetch("/api/myform", {
+    //     //     method: "POST",
+    //     //     headers: {
+    //     //         "Content-Type": "application/json",
+    //     //     },
+    //     //     body: JSON.stringify(candidat),
+    //     // }).then(res=>res.json()).then(data=>setUser(data.user))
+    // };
+
+    async function onSubmit(e) {
+        e.preventDefault();
+
+        var action = e.target.action;
+
+        var userData = JSON.stringify({
+            email: e.target.email.value,
+            name: e.target.name.value,
+            username: e.target.username.value,
+            password: e.target.password.value,
+            passwordConfirmation: e.target.passwordConfirmation.value
+        })
+
+        const response = axios.post('http://localhost:3005/auth/register', userData, {
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+
+        return response;
+
+    }
+
     return (
         <div className="flex min-h-full flex-col bg-white">
             <div className="flex min-h-full flex-1">
@@ -19,22 +66,26 @@ export default function Login()
 
                         <div className="mt-10">
                             <div>
-                                <form action="#" method="POST" className="space-y-6">
+                                <form onSubmit={onSubmit} action="http://localhost:3005/auth/register"  method="POST" className="space-y-6">
                                     <div>
-                                        <label htmlFor="email"
-                                               className="block text-sm font-medium leading-6 text-gray-900">E-posta
-                                            adresi</label>
+                                        <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">E-posta adresi</label>
                                         <div className="mt-2">
-                                            <input id="email" name="email" type="email" autoComplete="email" required=""
+                                            <input id="email" name="email" type="text" autoComplete="email" className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"/>
+                                        </div>
+                                    </div>
+
+                                    <div>
+                                        <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">Tam adınız</label>
+                                        <div className="mt-2">
+                                            <input id="name" name="name" type="text" autoComplete="name"
                                                    className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"/>
                                         </div>
                                     </div>
 
                                     <div>
-                                        <label htmlFor="username"
-                                               className="block text-sm font-medium leading-6 text-gray-900">Kullanıcı adı</label>
+                                        <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">Kullanıcı adı</label>
                                         <div className="mt-2">
-                                            <input id="email" name="email" type="email" autoComplete="email" required=""
+                                            <input id="username" name="username" type="text" autoComplete="username"
                                                    className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"/>
                                         </div>
                                     </div>
@@ -44,17 +95,15 @@ export default function Login()
                                         <label htmlFor="password"
                                                className="block text-sm font-medium leading-6 text-gray-900">Şifre</label>
                                         <div className="mt-2">
-                                            <input id="password" name="password" type="password"
-                                                   autoComplete="current-password" required=""
+                                            <input id="password" name="password" type="password" autoComplete="password"
                                                    className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"/>
                                         </div>
                                     </div>
                                     <div>
-                                        <label htmlFor="repassword"
+                                        <label htmlFor="passwordConfirmation"
                                                className="block text-sm font-medium leading-6 text-gray-900">Tekrar şifre</label>
                                         <div className="mt-2">
-                                            <input id="repassword" name="repassword" type="password"
-                                                   autoComplete="current-password" required=""
+                                            <input id="passwordConfirmation" name="passwordConfirmation" type="password" autoComplete="passwordConfirmation"
                                                    className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"/>
                                         </div>
                                     </div>
