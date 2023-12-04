b4b5b3d4f7fc756f0eb9cd2be136d9083592940b
Ravan <iamdinarbey@gmail.com>
2023-12-04 03:27:27 +0300

    added authentication middleware for verifying user sessions

--- client/src/middleware/isAuth.js
+++ client/src/middleware/isAuth.js
@@ -1,9 +1,35 @@
-export const isAuth = (cookies) => {
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+import axios from 'axios';
 
-    if (cookies.has('accessToken')) {
-        return true;
-    }
+const isAuth = (WrappedComponent) => {
+    const Auth = (props) => {
+        const router = useRouter();
 
-    return false;
+        useEffect(() => {
+            const token = localStorage.getItem('token');
 
-}
+            const verifyToken = async () => {
+                try {
+                    if (token) {
+                        const response = await axios.post('http://localhost:3005/auth/verifyToken', { token });
+                        if (response.data.valid) {
+                            return;
+                        }
+                    }
+                    router.push('/login');
+                } catch (error) {
+                    console.error('Token verification error:', error);
+                    router.push('/login');
+                }
+            };
+
+        }, []);
+
+        return <WrappedComponent {...props} />;
+    };
+
+    return Auth;
+};
+
+export default isAuth;
