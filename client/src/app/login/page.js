416638f0f9a86151a1f073547db55ed594b31111
Ravan <iamdinarbey@gmail.com>
2023-12-08 02:14:26 +0300

    added refreshToken

--- client/src/app/login/page.js
+++ client/src/app/login/page.js
@@ -44,6 +44,7 @@ export default function Login()
                 }
 
                 localStorage.setItem('token', response.accessToken);
+                localStorage.setItem('refreshToken', response.refreshToken);
 
                 router.push('/');
 
