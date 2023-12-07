888639fad1c47ad088d702e9285406f5493b59b3
Ravan <iamdinarbey@gmail.com>
2023-12-08 02:15:15 +0300

    added reactStrictMode and env

--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: true,
     headers: {
         'Access-Control-Allow-Origin': '*'
+    },
+    env: {
+        SERVER_URL: process.env.SERVER_URL,
     }
 }
 
