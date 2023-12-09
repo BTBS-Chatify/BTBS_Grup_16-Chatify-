6d18166eea4b0ec45b8e4cbed4c9b9a9abffe001
Enes Öztekin <enesoztekinofficial@gmail.com>
2023-12-09 15:26:47 +0300

    /all route added for fetching groups from db

--- server/controllers/group.js
+++ server/controllers/group.js
@@ -1,28 +1,56 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-var route = express.Router();
+const express = require("express");
+let route = express.Router();
 
 const { validate } = require("express-validation");
-const groupValidation = require('../validations/groupValidations');
+const groupValidation = require("../validations/groupValidations");
 
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
-route.post('/create', validate(groupValidation.create, {}, {}), async function (req, res)
-{
+route.post(
+  "/create",
+  validate(groupValidation.create, {}, {}),
+  async function (req, res) {
     const { name, userId } = req.body;
+
     await prisma.group.create({
-        data: {
-            name: name,
-            userId: userId
-        }
-    })
+      data: {
+        name: name,
+        userId: userId,
+      },
+    });
+
     return res.status(200).json({
-        status: "success",
-        message: "Grup oluşturuldu",
-    })
-})
+      status: "success",
+      message: "Grup oluşturuldu",
+    });
+  }
+);
+
+route.post("/all", async function (req, res) {
+  const { userId } = req.body; // userId'yi doğrudan req.body'den almalısınız, destructuring kullanımı yanlış
+  try {
+    const groups = await prisma.group.findMany({
+      where: {
+        userId: userId,
+      },
+    });
+
+    return res.status(200).json({
+      status: "success",
+      message: "Gruplar listelendi",
+      groups: groups,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: "error",
+      message: "Gruplar getirilirken bir hata oluştu",
+      error: error.message,
+    });
+  }
+});
 
 module.exports = route;
