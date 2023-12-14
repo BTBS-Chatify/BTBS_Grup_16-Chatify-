24fe58c94581108104119bb98afea81c343bc5b3
Enes Öztekin <enesoztekinofficial@gmail.com>
2023-12-14 23:11:33 +0300

    added friend route to server

--- server/app.js
+++ server/app.js
@@ -1,37 +1,37 @@
-const express           = require('express');
-const bodyParser        = require('body-parser');
-const { ValidationError } = require('express-validation');
+const express = require("express");
+const bodyParser = require("body-parser");
+const { ValidationError } = require("express-validation");
 const app = express();
 
-app.use(bodyParser.json()) // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: false })) // for parsing application/x-www-form-urlencoded
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: false })); // for parsing application/x-www-form-urlencoded
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    if (req.method === "OPTIONS") {
-        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-        return res.status(200).json({});
-    }
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
 });
 
-app.use('/auth', require('./controllers/auth'));
-app.use('/group', require('./controllers/group'));
+app.use("/auth", require("./controllers/auth"));
+app.use("/group", require("./controllers/group"));
+app.use("/friend", require("./controllers/friend"));
 
 app.use(function (err, req, res) {
-    if (err instanceof ValidationError) {
-        return res.status(err.statusCode).json(err)
-    }
-    return res.status(400).json(err)
-})
-
+  if (err instanceof ValidationError) {
+    return res.status(err.statusCode).json(err);
+  }
+  return res.status(400).json(err);
+});
 
 const port = 3005;
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
---
+++ server/controllers/friend.js
@@ -0,0 +1,39 @@
+"use strict";
+const express = require("express");
+var route = express.Router();
+
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+route.post("/friends", async function (req, res) {
+  const { userId } = req.body;
+  const friends = await prisma.friend.findMany({
+    where: {
+      user1Id: userId,
+    },
+    include: {
+      user2: {
+        select: {
+          username: true,
+          picture: true,
+        },
+      },
+    },
+  });
+
+  if (!friends) {
+    return res.status(400).json({
+      status: "error",
+      message: "Friends not found",
+    });
+  }
+
+  return res.status(200).json({
+    status: "success",
+    message: "Friends listelendi",
+    friends: friends,
+  });
+});
+
+module.exports = route;
